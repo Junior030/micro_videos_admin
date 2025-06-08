2514c2f2d629b7540f222fe42ccaf11424144c50
Junior030 <juniorhenrique030@gmail.com>
2025-06-08 13:45:40 +0000

    feat: adicionar entidade Category com propriedades e métodos relacionados

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,67 @@
+export type CategoryConstructoProps = {
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+};
+
+export class Category {
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(props: CategoryConstructoProps) {
+    this.category_id = props.category_id;
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.createdAt = props.createdAt ?? new Date();
+    this.updatedAt = props.updatedAt ?? new Date();
+  }
+
+  static create(props: CategoryCreateCommand): Category {
+    return new Category(props);
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+    this.updatedAt = new Date();
+  }
+
+  changeDescription(description: string | null): void {
+    this.description = description;
+    this.updatedAt = new Date();
+  }
+
+  activate(is_active: boolean): void {
+    this.is_active = is_active;
+    this.updatedAt = new Date();
+  }
+
+  deactivate(): void {
+    this.is_active = false;
+    this.updatedAt = new Date();
+  }
+
+  toJSON(): CategoryConstructoProps {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
+}
