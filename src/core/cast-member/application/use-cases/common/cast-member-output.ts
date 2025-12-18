e705ff660db8c486b0764a573ed42c9ad8fd187e
Junior030 <juniorhenrique030@gmail.com>
2025-12-18 19:55:01 +0000

    feat: implement ListCastMembers use case with input validation and output mapping
    - Add ListCastMembersUseCase to handle listing cast members with pagination, filtering, and sorting.
    - Create ListCastMembersInput and ListCastMembersFilter classes for input validation.
    - Implement toOutput method for formatting the output.
    - Add unit tests for ListCastMembersUseCase covering various scenarios.
    
    feat: implement UpdateCastMember use case with validation and error handling
    
    - Add UpdateCastMemberUseCase to manage updating cast member details.
    - Create UpdateCastMemberInput for input validation.
    - Implement error handling for not found entities and validation errors.
    - Add integration and unit tests for UpdateCastMemberUseCase.
    
    test: add CastMembersIdExistsInDatabaseValidator for validating cast member IDs
    
    - Implement CastMembersIdExistsInDatabaseValidator to check existence of cast member IDs in the database.
    - Add unit tests to verify the behavior of the validator.
    
    refactor: enhance InMemoryRepository with findByIds and existsById methods
    
    - Implement findByIds and existsById methods in InMemoryRepository for better entity management.
    - Update existing methods to improve error handling and code clarity.

---
+++ src/core/cast-member/application/use-cases/common/cast-member-output.ts
@@ -0,0 +1,19 @@
+import { CastMember } from '../../../domain/cast-member.aggregate';
+
+export type CastMemberOutput = {
+  id: string;
+  name: string;
+  type: number;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export class CastMemberOutputMapper {
+  static toOutput(entity: CastMember): CastMemberOutput {
+    const { cast_member_id, ...other_props } = entity.toJSON();
+    return {
+      id: cast_member_id,
+      ...other_props,
+    };
+  }
+}
